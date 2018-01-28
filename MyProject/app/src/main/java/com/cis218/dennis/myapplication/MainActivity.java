20332698723e2d4c6223b49c07e585df8dfdadb6
Dennis Sauve <dengsauve@yahoo.com>
2018-01-27 19:59:50 -0800

    Completed 1-22 Lecture

--- MyProject/app/src/main/java/com/cis218/dennis/myapplication/MainActivity.java
+++ MyProject/app/src/main/java/com/cis218/dennis/myapplication/MainActivity.java
@@ -2,12 +2,26 @@ package com.cis218.dennis.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    EditText edtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById( R.id.btn1 );
+        edtResult = (EditText)findViewById( R.id.edtResult );
+    }
+
+    public void btnOnClick( View v ){
+        String currentResult = edtResult.getText().toString();
+        edtResult.setText(currentResult + "1");
     }
 }
